2f759b53ddfd57b8011172127cc457ecb573e242
Tinturi <artkorch28@gmail.com>
2026-09-02 13:17:31 +0700

    Set Turbo League rating to +25/-25

--- lib/rating.ts
+++ lib/rating.ts
@@ -1,6 +1,6 @@
 export const START_RATING = 1000;
-export const WIN_DELTA = 20;
-export const LOSS_DELTA = -20;
+export const WIN_DELTA = 25;
+export const LOSS_DELTA = -25;
 
 export function ratingDelta(won: boolean) {
   return won ? WIN_DELTA : LOSS_DELTA;
