b7081abe26d92cbefbbe2d506c504d7966bb3d6f
Tinturi <artkorch28@gmail.com>
2026-09-04 03:24:38 +0700

    Add Season 3 configuration

---
+++ lib/season.ts
@@ -0,0 +1,6 @@
+export const CURRENT_SEASON = 3;
+export const SEASON_START_ISO = "2026-09-03T20:18:00.000Z";
+export const SEASON_START_LABEL = "04.09.2026 03:18";
+export const CALIBRATION_MATCHES = 5;
+export const CALIBRATION_DELTA = 50;
+export const REGULAR_DELTA = 25;
